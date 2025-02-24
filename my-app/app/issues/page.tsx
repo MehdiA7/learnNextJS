08f8993d0118940381fb8f2f02deaa99c411f298
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-02-24 14:35:22 +0100

    Add prisma

--- my-app/app/issues/page.tsx
+++ my-app/app/issues/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import NavBar from '../components/NavBar';
+import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return (
-    <div>
-        <NavBar/>
-        <h1>Issues Page</h1>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1>Issues Page</h1>
+            <Button>Add Issues</Button>
+        </div>
+    );
+};
 
 export default IssuesPage;
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
-import NavBar from "./components/NavBar";
 
 
 export default function Home() {
     return (
         <main>
-            <NavBar />
             <Link href="/users">Users</Link>
             <ProductCard />
         </main>
