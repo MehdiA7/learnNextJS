89b53f4984dcb37f34fe46917ca565bcc979e04e
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-02-24 16:04:14 +0100

    Create Issues

---
+++ my-app/app/api/issues/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+// Zod is a TypeScript-first schema declaration and validation library.
+import { z } from 'zod';
+// for db interaction
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+// format de rule for the body request
+const createIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1)
+});
+
+// create a post function
+export async function POST(request: NextRequest) {
+    // get the body
+    const body = await request.json();
+    // verify if the body are good
+    const validation = createIssueSchema.safeParse(body);
+    if(!validation.success){
+        return NextResponse.json(validation.error.errors, {status: 400});
+    };
+    // send the information to the database
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description }
+    });
+    return NextResponse.json(newIssue, {status: 201});
+}
+
