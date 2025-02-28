87c10588dc4a995b29f23136b6aafb5c1390faf9
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-02-28 14:21:40 +0100

    Add loading for new issue form

--- my-app/app/issues/new/loading.tsx
+++ my-app/app/issues/new/loading.tsx
@@ -1,10 +1,16 @@
-import { Spinner } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
 import React from "react";
 
 const LoadingNewIssuePage = () => {
     return (
-        <div>
-            <Spinner size={"3"} />
+        <div className="max-w-xl space-y-3">
+            <Skeleton className="mb-4" width="8rem"/>
+            <Skeleton />
+            <Skeleton height="25rem"/>
+            <Button >
+                Submit New Issue
+            </Button>
         </div>
     );
 };
--- my-app/app/issues/new/page.tsx
+++ my-app/app/issues/new/page.tsx
@@ -1,21 +1,28 @@
 "use client";
 import React, { useState } from "react";
 // Radix ui theme
-import { Button, TextField, Callout, Text } from "@radix-ui/themes";
+import { Button, TextField, Callout, Text, Heading } from "@radix-ui/themes";
+
 // Markdown editor
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+
 // React use form for simple form request
 import { useForm, Controller } from "react-hook-form";
+
 // Axios for make some better request
 import axios from "axios";
+
 // Router to change page after the post request
 import { useRouter } from "next/navigation";
+
 // Form error checker
 import { zodResolver } from "@hookform/resolvers/zod";
 import { date, z } from "zod";
+
 // Schema for checker
 import createIssueSchema from "@/app/validationSchemas";
+
 // Components
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
@@ -23,6 +30,7 @@ import Spinner from "@/app/components/Spinner";
 type issueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
+
     const [error, setError] = useState("");
     const [isSubmit, setIsSubmit] = useState(false);
     const {
@@ -53,6 +61,7 @@ const NewIssuePage = () => {
                     <Callout.Text>{error}</Callout.Text>
                 </Callout.Root>
             )}
+            <Heading className="mb-4">New Issue</Heading>
             <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
                 <TextField.Root
                     placeholder="title"
