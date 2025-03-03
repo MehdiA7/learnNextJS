750b05e7cfcf50b635578f7819296dbea64abc39
MehdiA7 <152538807+MehdiA7@users.noreply.github.com>
2025-03-03 17:07:01 +0100

    save

--- my-app/app/issues/[id]/edit/page.tsx
+++ my-app/app/issues/[id]/edit/page.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import IssueForm from "../../../components/IssueForm";
 import prisma from "@/prisma/client";
+import { use } from "react";
 
 type Props = {
-    params: { id: string };
-}
+    params: Promise<{ id: string }>;
+};
 
-const EditIssuePage = async ({ params }: Props) => {
-    const { id } = params;
-    const issue = await prisma.issue.findUnique({
+const EditIssuePage = ({ params }: Props) => {
+    const { id } = use(params);
+
+    const issue = use(prisma.issue.findUnique({
         where: { id: parseInt(id) },
-    });
+    }));
+
     if (!issue) {
         return <div>Issue not found</div>;
     }
+
     return <IssueForm issue={issue} />;
 };
 
